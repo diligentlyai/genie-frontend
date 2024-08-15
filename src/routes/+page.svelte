f30603328a7f0186ad35cdbfb5a3eed9077b905f
Jake Lanning <lanning.jake.w@gmail.com>
2024-08-15 06:20:44 -0400

    add some buttons and mess with spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,13 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<form class="p-5 mx-auto max-w-screen-md">
-	<section class=" pl-16 p-5 relative">
-		<div class="absolute top-6 left-4 w-8 aspect-square bg-secondary"></div>
+<form class="p-5 mx-auto max-w-screen-md space-y-3">
+	<section class=" pl-16 p-5 relative space-y-4">
 		<h1 class="text-4xl font-bold">Diligently AI</h1>
-		<h3 class="mt-4">Lead generation focused on increasing quality over quantity.</h3>
+		<h3>Lead generation focused on increasing quality over quantity.</h3>
+		<div class="absolute top-2 left-4 w-8 aspect-square bg-secondary"></div>
 	</section>
-	<section>
+	<section class="space-y-2">
 		<h4 class="text-2xl font-medium">Contact Details</h4>
 		<div class="flex flex-row gap-5">
 			<div class="w-[40%]">
@@ -25,15 +25,24 @@
 			</div>
 		</div>
 	</section>
-	<section class="mt-5">
+	<section>
 		<Tabs />
 	</section>
-	<section>
-		Provide starting account list
+	<section class="space-y-2">
+		<h4>Provide starting account list</h4>
 		<Filepicker />
 	</section>
-	<section class="ml-auto">
-		<Button>Find accounts and contacts</Button>
-		<Button>Find accounts</Button>
+	<section class="block space-y-2">
+		<Button class="block ml-auto">Find accounts and contacts</Button>
+		<Button class="block ml-auto">Find accounts</Button>
+		<p>
+			<span class="ml-auto block w-fit"
+				>Because we get real time updated information, we take a few days to get results.</span
+			>
+			<span class="ml-auto block w-fit"
+				>If you need more than 50 matching accounts just email ben@diligently.ai to get billing set
+				up.</span
+			>
+		</p>
 	</section>
 </form>
