6a772cbf1af0a36162212748d9b0f7d5a9ec860f
Jake Lanning <lanning.jake.w@gmail.com>
2024-08-15 01:50:35 -0400

    start to turn page into a form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script>
-    import "../app.css";
-  </script>
-  
-  <div class="bg-primary text-secondary h-screen">
-    <slot />
-  </div>
-  
+	import '../app.css';
+</script>
+
+<div class="h-screen">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,25 @@
-<h1 class="text-4xl p-10 font-bold">Welcome to SvelteKit</h1>
-<p class="text-xl pl-10">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Label } from '$lib/components/ui/label';
+	import { Input } from '$lib/components/ui/input';
+</script>
 
+<form class="p-5 mx-auto max-w-screen-md">
+	<section class=" pl-16 p-5 relative">
+        <div class="absolute top-6 left-4 w-8 aspect-square bg-secondary"></div>
+		<h1 class="text-4xl font-bold">Diligently AI</h1>
+		<h3 class="mt-4">Lead generation focused on increasing quality over quantity.</h3>
+	</section>
+	<section>
+		<h4 class="text-2xl font-medium">Contact Details</h4>
+		<div class="flex flex-row gap-5">
+			<div class="w-[40%]">
+				<Label for="name">Name</Label>
+				<Input id="name" name="name" />
+			</div>
+			<div class="w-[60%]">
+				<Label for="email">Email</Label>
+				<Input id="email" name="email" />
+			</div>
+		</div>
+	</section>
+</form>
