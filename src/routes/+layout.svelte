331e50e972ce65e1ae15af6d2b573b09bbc56ba6
Ben Parfitt <ben@diligently.ai>
2024-08-18 22:06:33 -0400

    tagline

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 <div class="h-screen p-5 mx-auto max-w-screen-xl space-y-3">
 	<section class="pl-16 p-5 relative space-y-4">
 		<h1 class="text-4xl font-bold">Diligently AI</h1>
-		<h3>Lead generation focused on increasing quality over quantity.</h3>
+		<h3>Lead generation focused on <b>legacy industries</b>.</h3>
 		<img src="/favicon.png" class="absolute top-1 left-4 w-10 aspect-square" alt="logo" />
 	</section>
 	<slot />
